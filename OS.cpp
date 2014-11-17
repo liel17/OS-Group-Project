180f25d962b53727e33c206f063951beff8286fa
liel17 <liel17@aol.com>
2014-11-17 11:28:49 -0500

    Function signatures

---
+++ OS.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+using namespace std;
+
+void siodisk(int jobnum);
+void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
+void ontrace();
+void offtrace();
+void startup();
+void Crint(int a, int p[]);
+void Dskint(int a, int p[]);
+void Drmint(int a, int p[]);
+void Tro(int a, int p[]);
+void Svc(int a, int p[]);
+
+
+int main() {
+
+
+
+
+
+return 0;
+}
+void siodisk(int jobnum){
+
+};
+void siodrum(int jobnum, int jobsize, int coreaddress, int direction){
+
+};
+void ontrace(){
+
+
+};
+void offtrace(){
+
+
+};
+void startup(){
+
+
+};
+void Crint(int a, int p[]){
+
+
+};
+void Dskint(int a, int p[]){
+
+
+};
+void Drmint(int a, int p[]){
+
+
+};
+void Tro(int a, int p[]){
+
+
+};
+void Svc(int a, int p[]){
+
+
+};
